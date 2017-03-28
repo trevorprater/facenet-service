685da15fd8b3774c4562c13b75e71108498a548d
Trevor Prater <trevor.prater@enigma.io>
2017-03-27 21:09:50 -0400

    Init commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='facenet-service',
+      version='0.1',
+      description='Face Recognition as a service using Tensorflow',
+      author='Youfie Technologies Inc',
+      author_email='trevor@youfie.io',
+      url='https://github.com/trevorprater/facial/',
+      packages=['src', 'src.align', 'src.models', 'test'], )
